9b17e75d58e0d93f26bc5f3293be62194646f7fd
Hari Surendran <harisurendran@gmail.com>
2016-07-04 17:53:06 -0500

    committing java project

---
+++ java-scala/src/com/hs/java/Main.java
@@ -0,0 +1,14 @@
+package com.hs.java;
+
+import com.hs.scala.ScalaExample;
+
+public class Main {
+	
+	public static void main(String[] args){
+		
+		System.out.println("Hello");
+		 new ScalaExample().hello();
+		 ScalaExample.helloObject(); 
+	}
+
+}
---
+++ java-scala/src/com/hs/scala/ScalaExample.scala
@@ -0,0 +1,9 @@
+package com.hs.scala
+
+class ScalaExample {
+  def hello() { println("Hello (class)") } // [1]
+}
+object ScalaExample {
+   
+  def helloObject() { println("Hello Object") } // [3]
+}
